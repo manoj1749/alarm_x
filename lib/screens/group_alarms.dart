a987eae56d9cd0eb14f369daf25f77979c1b7440
rv602 <rajagopalan602@gmail.com>
2023-04-15 21:54:50 +0530

    refactored file structure

---
+++ lib/screens/group_alarms.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class GroupAlarm extends StatefulWidget {
+  const GroupAlarm({super.key});
+
+  @override
+  State<GroupAlarm> createState() => _GroupAlarmState();
+}
+
+class _GroupAlarmState extends State<GroupAlarm> {
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Group Alarm'),
+    );
+  }
+}
