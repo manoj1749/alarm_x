8e5da4aa01529338605fe5ce94901282463fee92
Manoj <manojparasuram.sadhanala@gmail.com>
2023-04-15 17:10:21 +0530

    updated test widget

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,12 +8,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:alarm_x/main.dart';
+import 'package:alarm_x/screens/home.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const ExampleAlarmHomeScreen());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
