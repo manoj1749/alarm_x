96a455456dba4fc5a842c8b8891c80863dfd7392
Manoj <manojparasuram.sadhanala@gmail.com>
2023-04-16 09:05:40 +0530

    updated login page

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -2,9 +2,9 @@
 
 import 'dart:math';
 
+import 'package:alarm_example/utils/animatedpage.dart';
 import 'package:flutter/material.dart';
 
-import '../utils/animatedpage.dart';
 import 'home.dart';
 
 class LoginPage extends StatelessWidget {
@@ -16,138 +16,51 @@ class LoginPage extends StatelessWidget {
       decoration: const BoxDecoration(color: Colors.white),
       child: Stack(
         children: <Widget>[
-          Positioned.fill(
-              child: AnimatedWave(
-            height: 100,
+          Positioned.fill(child: AnimatedBackground()),
+          onBottom(AnimatedWave(
+            height: 180,
             speed: 1.0,
           )),
           onBottom(AnimatedWave(
-            height: 250,
-            speed: 1.1,
-          )),
-          onBottom(AnimatedWave(
-            height: 300,
-            speed: 1.2,
+            height: 120,
+            speed: 0.9,
             offset: pi,
           )),
           onBottom(AnimatedWave(
-            height: 400,
-            speed: 1.3,
+            height: 220,
+            speed: 1.2,
             offset: pi / 2,
           )),
-          //const Positioned.fill(child: Center(child: FlutterLogo(size: 150))),
-          Center(
+          Positioned.fill(
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 const Text(
                   'Welcome to the Alarm App',
                   style: TextStyle(
-                      fontSize: 20,
-
-                      //fontWeight: FontWeight.bold,
-                      color: Colors.black,
-                      decoration: TextDecoration.none),
-                ),
-                const SizedBox(height: 32),
-                ElevatedButton.icon(
-                  style: ButtonStyle(
-                    backgroundColor: MaterialStateProperty.resolveWith<Color?>(
-                      (Set<MaterialState> states) {
-                        return const Color.fromARGB(255, 162, 157, 150);
-                      },
-                    ),
+                    fontSize: 20,
+                    color: Colors.white,
+                    decoration: TextDecoration.none,
                   ),
+                ),
+                const SizedBox(height: 35),
+                FloatingActionButton.extended(
                   onPressed: () {
                     Navigator.of(context).pushReplacement(
-                      MaterialPageRoute(
-                        builder: (context) => const HomePage(),
-                      ),
+                      MaterialPageRoute(builder: (_) => const HomePage()),
                     );
                   },
-                  icon: const Icon(Icons.home),
-                  label: const Text('Go to Home Page'),
+                  label: const Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Text('Continue'),
+                  ),
+                  icon: const Icon(Icons.arrow_forward),
+                  backgroundColor: const Color.fromARGB(50, 24, 138, 130),
+                  foregroundColor: Colors.white,
                 ),
-                // ElevatedButton.icon(
-                //   style: ButtonStyle(backgroundColor:
-                //       MaterialStateProperty.resolveWith<Color?>(
-                //           (Set<MaterialState> states) {
-                //     return const Color.fromARGB(255, 162, 157, 150);
-                //   })),
-                //   onPressed: () async {
-                //     final bool userConnection =
-                //         await CheckUserConnection().checkUserConnection();
-                //     if (userConnection) {
-                //       await Authentication.initializeFirebase();
-                //       User? user = await Authentication.signInWithGoogle(
-                //           context: context);
-                //       print('1');
-                //       print(user);
-                //       print('2');
-
-                //       Navigator.of(context).pushReplacement(
-                //         MaterialPageRoute(
-                //           builder: (context) => const HomePage(),
-                //         ),
-                //       );
-                //     } else if (!userConnection) {
-                //       ScaffoldMessenger.of(context).showSnackBar(
-                //         snackBars.customSnackBar(
-                //           content: 'No internet connection',
-                //         ),
-                //       );
-                //     } else {
-                //       ScaffoldMessenger.of(context).showSnackBar(
-                //         snackBars.customSnackBar(
-                //           content: 'Something went wrong',
-                //         ),
-                //       );
-                //     }
-                //   },
-                //   icon: const Icon(Icons.login),
-                //   label: const Text('Sign in with Google'),
-                // ),
-                // ElevatedButton.icon(
-                //   style: ButtonStyle(backgroundColor:
-                //       MaterialStateProperty.resolveWith<Color?>(
-                //           (Set<MaterialState> states) {
-                //     return const Color.fromARGB(255, 162, 157, 150);
-                //   })),
-                //   onPressed: () async {
-                //     final bool userConnection =
-                //         await CheckUserConnection().checkUserConnection();
-                //     if (userConnection) {
-                //       await Authentication.initializeFirebase();
-                //       await Authentication.signOut(context: context);
-                //       print('1');
-                //       print('signed Out');
-                //       //print(user);
-                //       print('2');
-                //       Navigator.of(context).pushReplacement(
-                //         MaterialPageRoute(
-                //           builder: (context) => const HomePage(),
-                //         ),
-                //       );
-                //     } else if (!userConnection) {
-                //       ScaffoldMessenger.of(context).showSnackBar(
-                //         snackBars.customSnackBar(
-                //           content: 'No internet connection',
-                //         ),
-                //       );
-                //     } else {
-                //       ScaffoldMessenger.of(context).showSnackBar(
-                //         snackBars.customSnackBar(
-                //           content: 'Something went wrong',
-                //         ),
-                //       );
-                //     }
-                //   },
-                //   icon: const Icon(Icons.login),
-                //   label: const Text('Sign Out'),
-                // ),
               ],
             ),
-          )
+          ),
         ],
       ),
     );
